1c18a6a4042443f9f9063ed7659237b9d96e5f06
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-10-18 14:58:01 +0200

    Fix modal window

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,33 +1,58 @@
 import Modal from 'react-modal'
-import styles from './ImageModal.module.css'
+// import styles from './ImageModal.module.css'
 
 const ImageModal = ({ isOpen, closeModal, modalData }) => {
-    const customStyles = {
-        content: {
-            display: "flex",
-            position: "relative",
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: "0",
-            padding: 0,
-            width: "500px",
-            height: "fit-content",
-            opacity: 1,
-            backgroundColor: "black",
-            color: "white",
-            inset: 0,
-        },
+    // const customStyles = {
+    //     content: {
+    //         display: "flex",
+    //         position: "relative",
+    //         justifyContent: "center",
+    //         alignItems: "center",
+    //         borderRadius: "0",
+    //         padding: 0,
+    //         width: "500px",
+    //         height: "fit-content",
+    //         opacity: 1,
+    //         backgroundColor: "rgba(144, 158, 189, 0.5)",
+    //         color: "white",
+    //         inset: 0,
+    //     },
 
-    }
+    // }
 
     return (
         <>
             <Modal
-                style={customStyles}
+                style={{
+                    overlay: {
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        zIndex: "999999",
+                        backgroundColor: "rgba(144, 158, 189, 0.5)",
+                        backdropFilter: "blur(5px)",
+                    },
+                    content: {
+                        display: "flex",
+                        position: "relative",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: "0",
+                        border: "none",
+                        padding: "0",
+                        width: "700px",
+                        height: "700px",
+                        overflow: "hidden",
+                        // height: "fit-content",
+                        opacity: 1,
+                        backgroundColor: "transparent",
+                        color: "white",
+                        inset: 0,
+                    },
+                }}
                 isOpen={isOpen}
                 onRequestClose={closeModal}
                 contentLabel="Image modal"
-                overlayClassName={styles.modalOverlay}
             >
                 <img src={modalData.urls.regular} alt={modalData.description} />
             </Modal>
